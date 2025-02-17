c55ad0b03a7c23fe18bb0f2ba58cfee3f675d0fc
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-17 13:51:39 +0600

    event handler added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ function App() {
   function handleClick() {
     alert("button clicked");
   }
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
   return (
     <>
       <h1>React Core Concepts</h1>
       <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => addToFive(3)}>add</button>
     </>
   );
 }
