5710993f7b56fb8ba93ae066a7a526ad388d049b
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-17 14:37:04 +0600

    reduce handler added

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,10 +6,15 @@ export default function Counter() {
     const newCount = count + 1;
     setCount(newCount);
   };
+  const handleReduce = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  };
   return (
     <div style={{ border: "2px solid purple" }}>
       <h3>Counter:{count}</h3>
       <button onClick={handleAdd}>Add</button>
+      <button onClick={handleReduce}>Reduce</button>
     </div>
   );
 }
